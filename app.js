0c107a29d30bccb4f1d4c263930aa50b1e67cc7d
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 17:21:11 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello! sir Zia");
+let message = "Bisma Digitalisma has this side";
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,4 @@
+console.log("Hello! sir Zia");
+let message="Bisma Digitalisma has this side";
+console.log(message);
+//So I created all the files mwntioned for the step00_helloworld task
